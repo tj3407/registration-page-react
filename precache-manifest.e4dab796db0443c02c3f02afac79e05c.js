3938d2a75e6ebb73d73429418556dcdb47140ce8
TJ Sornet <tsornet@gmail.com>
2020-09-06 08:59:03 -0700

    Updates

---
+++ precache-manifest.e4dab796db0443c02c3f02afac79e05c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6c559f735857d66c0c28db29345c01",
+    "url": "/registration-page-react/index.html"
+  },
+  {
+    "revision": "762fb1aba6234fd2d9b6",
+    "url": "/registration-page-react/static/css/main.ef37c5a3.chunk.css"
+  },
+  {
+    "revision": "3777b7817ac8c6801985",
+    "url": "/registration-page-react/static/js/2.aa3aedb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/registration-page-react/static/js/2.aa3aedb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762fb1aba6234fd2d9b6",
+    "url": "/registration-page-react/static/js/main.c0a0167e.chunk.js"
+  },
+  {
+    "revision": "5a06d32196855eeb631e",
+    "url": "/registration-page-react/static/js/runtime-main.265fd626.js"
+  },
+  {
+    "revision": "68b8933b20db31cfe6e5a0f16a19e73d",
+    "url": "/registration-page-react/static/media/logo-desktop.68b8933b.svg"
+  }
+]);
